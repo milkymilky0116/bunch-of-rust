7c8d3b9e8a14661297d1e9b33dcf198e575bf230
MangoSteen0903 <sjlee990129@gmail.com>
2023-12-02 16:33:11 +0900

    feat: #1 guessing game self-implementation

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,34 +1,48 @@
-extern crate rand;
 use rand::Rng;
 use std::{cmp::Ordering, io};
+
 fn main() {
     println!("Guess the number!");
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
-
-    //println!("The secret number is : {}", secret_number);
+    let answer = rand::thread_rng().gen_range(1, 101);
+    let mut trial = 10;
+    println!("Randomly generated number is : {}", answer);
     loop {
-        println!("Please input your guess.");
-
+        println!("Please input your guess");
         let mut guess = String::new();
-
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Failed to read line");
+        match io::stdin().read_line(&mut guess) {
+            Ok(_) => println!("the number that you entered is : {}", guess),
+            Err(_) => println!("Please Enter proper input."),
+        }
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => continue,
         };
-        println!("You guessed: {}", guess);
 
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
+        match guess.cmp(&answer) {
+            Ordering::Greater => {
+                println!("your number is greater than answer");
+                trial -= 1;
+                println!("Remain trial : {}", trial);
+            }
+            Ordering::Less => {
+                println!("your number is less than answer");
+                trial -= 1;
+                println!("Remain trial : {}", trial);
+            }
             Ordering::Equal => {
-                println!("You win!");
+                println!("Correct!");
                 break;
             }
+        }
+
+        match trial {
+            0 => {
+                println!("You're out of trial! Actual answer : {}", answer);
+                break;
+            }
+            _ => continue,
         }
     }
 }
