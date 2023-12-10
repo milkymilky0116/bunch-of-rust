e989b1e86f885f7345f4ef50a4c7bb121b95f1b8
milkymilky0116 <sjlee990129@gmail.com>
2023-12-10 17:33:58 +0900

    feat: #2 web scraper, concurrency

--- web_scrapper/src/main.rs
+++ web_scrapper/src/main.rs
@@ -1,28 +1,128 @@
-use std::collections::HashMap;
+use std::{
+    sync::{mpsc, Arc, Mutex},
+    thread,
+};
 
-use reqwest::StatusCode;
+use scraper::Html;
+struct PokemonProduct {
+    url: Option<String>,
+    image: Option<String>,
+    name: Option<String>,
+    price: Option<String>,
+}
+const BASE_URL: &str = "https://scrapeme.live/shop";
+fn main() {
+    let total_pages = get_pages(&BASE_URL.to_string());
+    let products: Vec<PokemonProduct> = vec![];
 
-#[tokio::main]
-async fn main() -> Result<(), reqwest::Error> {
-    let mut urls: Vec<String> = Vec::new();
-    let mut url_status: HashMap<&String, StatusCode> = HashMap::new();
+    let products_mutex = Arc::new(Mutex::new(products));
 
-    urls.push(String::from("https://naver.com"));
-    urls.push(String::from("https://google.com"));
-    urls.push(String::from("https://github.com"));
-    urls.push(String::from("http://naver.com/asdad"));
+    let (tx, rx) = mpsc::channel::<Vec<PokemonProduct>>();
+    let mut handles = vec![];
+    for i in 1..total_pages + 1 {
+        let tx_clone = mpsc::Sender::clone(&tx);
+        let handle = thread::spawn(move || {
+            let url = format!("{}/page/{}/", BASE_URL, i);
+            println!("Scraping: {}", url);
+            let pokemon_product = scrap_page(&url);
+            tx_clone.send(pokemon_product).unwrap();
+        });
+        handles.push(handle);
+    }
+    for handle in handles {
+        handle.join().unwrap();
+    }
+    let receives_products: Vec<PokemonProduct> = rx.try_iter().flatten().collect();
 
-    for (_, element) in urls.iter().enumerate() {
-        let resp = reqwest::get(element).await?;
-        let status = resp.status();
-        url_status.insert(element, status);
-        println!("{}", status);
+    let mut locked_products = products_mutex.lock().unwrap();
+    locked_products.extend(receives_products);
+    write_csv(&locked_products);
+}
+
+fn parse_html_document(url: &String) -> Html {
+    let response = reqwest::blocking::get(url);
+    let html_content = response.unwrap().text().unwrap();
+
+    let document = scraper::Html::parse_document(&html_content);
+
+    document
+}
+
+fn get_pages(url: &String) -> i32 {
+    let document = parse_html_document(url);
+    let page_selector = scraper::Selector::parse("ul.page-numbers li:nth-last-child(2)").unwrap();
+    let html_pages = document
+        .select(&page_selector)
+        .next()
+        .map(|li| li.text().collect::<String>())
+        .unwrap()
+        .parse::<i32>()
+        .unwrap();
+
+    html_pages
+}
+
+fn scrap_page(url: &String) -> Vec<PokemonProduct> {
+    let document = parse_html_document(url);
+    let product_selector = scraper::Selector::parse("li.product").unwrap();
+    let html_products = document.select(&product_selector);
+
+    let mut pokemon_products: Vec<PokemonProduct> = Vec::new();
+
+    for product in html_products {
+        let url = product
+            .select(&scraper::Selector::parse("a").unwrap())
+            .next()
+            .and_then(|a| a.value().attr("href"))
+            .map(str::to_owned);
+        let image = product
+            .select(&scraper::Selector::parse("img").unwrap())
+            .next()
+            .and_then(|a| a.value().attr("src"))
+            .map(str::to_owned);
+
+        let name = product
+            .select(&scraper::Selector::parse("h2").unwrap())
+            .next()
+            .map(|h2| h2.text().collect::<String>());
+
+        let price = product
+            .select(&scraper::Selector::parse(".price").unwrap())
+            .next()
+            .map(|price| price.text().collect::<String>());
+
+        let pokemon_product = PokemonProduct {
+            url,
+            image,
+            name,
+            price,
+        };
+
+        pokemon_products.push(pokemon_product);
     }
 
-    for (key, value) in &url_status {
-        println!("URL: {}, Status: {}", key, value);
+    pokemon_products
+}
+
+fn write_csv(products: &Vec<PokemonProduct>) {
+    let path = std::path::Path::new("products.csv");
+    let mut writer = csv::Writer::from_path(path).unwrap();
+
+    writer
+        .write_record(&["index", "url", "image", "name", "price"])
+        .unwrap();
+
+    for (i, product) in products.iter().enumerate() {
+        let url = product.url.as_ref().unwrap();
+        let image = product.image.as_ref().unwrap();
+        let name = product.name.as_ref().unwrap();
+        let price = product.price.as_ref().unwrap();
+
+        writer
+            .write_record(&[&i.to_string(), url, image, name, price])
+            .unwrap();
     }
-    Ok(())
+    writer.flush().unwrap();
 }
 
 //https://weworkremotely.com/remote-jobs/search?term=python
